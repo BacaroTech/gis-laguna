d432aff138fc6b63da0eb22b00a2b36da546a289
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2026-05-16 17:00:17 +0200

    update angular 18

--- ProgettoLagunaFE/src/app/app.module.ts
+++ ProgettoLagunaFE/src/app/app.module.ts
@@ -3,23 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MapComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideHttpClient()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MapComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [
+        provideHttpClient(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
